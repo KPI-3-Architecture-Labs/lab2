fe4842e3d7ca69a07f194950751d7549c3dd301f
Antonio <antony.dovzhenko@gmail.com>
2024-03-16 19:48:22 +0200

    Added implementation for postfix evaluation function

--- implementation.go
+++ implementation.go
@@ -1,9 +1,52 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+type operationFunc func(a, b int) int
+
+func CalculatePostfix(input string) (string, error) {
+	var stack []int
+	operations := map[string]operationFunc{
+		"+": func(a, b int) int { return a + b },
+		"-": func(a, b int) int { return a - b },
+		"*": func(a, b int) int { return a * b },
+		"/": func(a, b int) int {
+			if b == 0 {
+				return 0
+			}
+			return a / b
+		},
+	}
+
+	tokens := strings.Fields(input)
+	for _, token := range tokens {
+		value, err := strconv.Atoi(token)
+		if err == nil {
+			stack = append(stack, value)
+		} else {
+			if len(stack) < 2 {
+				return "", fmt.Errorf("invalid expression: not enough operands")
+			}
+			a, b := stack[len(stack)-2], stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+
+			opFunc, exists := operations[token]
+			if !exists {
+				return "", fmt.Errorf("unsupported operator: %s", token)
+			}
+			result := opFunc(a, b)
+			stack = append(stack, result)
+		}
+	}
+
+	if len(stack) != 1 {
+		return "", fmt.Errorf("invalid expression: too many operands")
+	}
+
+	resultStr := fmt.Sprintf("%d", stack[0])
+	return resultStr, nil
 }
