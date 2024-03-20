b80275f8f25c7a9dcc43218e68df63c7666ba91c
Maksym Rybalko <maxymrybalko2005@gmail.com>
2024-03-20 22:46:39 +0200

    Refactored implementation tests

--- implementation_test.go
+++ implementation_test.go
@@ -16,46 +16,57 @@ type MySuite struct{}
 var _ = Suite(&MySuite{})
 
 func (s *MySuite) TestCalculatePostfix(cs *C) {
-
-	// simple tests
-
-	result, err := CalculatePostfix("2 2 +")
+	result, _ := CalculatePostfix("2 2 +")
 	cs.Assert(result, Equals, "4")
+}
 
-	result, err = CalculatePostfix("3 5 ^")
+func (s *MySuite) TestCalculatePostfixPower(cs *C) {
+	result, _ := CalculatePostfix("3 5 ^")
 	cs.Assert(result, Equals, "243")
+}
 
-	// more operands
-
-	result, err = CalculatePostfix("4 5 + 5 *")
+func (s *MySuite) TestCalculatePostfixMedium(cs *C) {
+	result, _ := CalculatePostfix("4 5 + 5 *")
 	cs.Assert(result, Equals, "45")
+}
 
-	result, err = CalculatePostfix("4 2 - 3 * 5 +")
+func (s *MySuite) TestCalculatePostfixComplex(cs *C) {
+	result, _ := CalculatePostfix("4 2 - 3 * 5 +")
 	cs.Assert(result, Equals, "11")
+}
 
-	result, err = CalculatePostfix("2 3 ^ 2 + 2 *")
+func (s *MySuite) TestCalculatePostfixComplexPower(cs *C) {
+	result, _ := CalculatePostfix("2 3 ^ 2 + 2 *")
 	cs.Assert(result, Equals, "20")
+}
 
-	// complex tests
-
-	result, err = CalculatePostfix("5 2 + 3 * 4 - 2 / 6 + 3 +")
+func (s *MySuite) TestCalculatePostfixBig(cs *C) {
+	result, _ := CalculatePostfix("5 2 + 3 * 4 - 2 / 6 + 3 +")
 	cs.Assert(result, Equals, "17")
+}
 
-	// validation tests
-
-	result, err = CalculatePostfix("")
+func (s *MySuite) TestCalculatePostfixEmptyError(cs *C) {
+	_, err := CalculatePostfix("")
 	cs.Assert(err, ErrorMatches, "invalid expression: empty input")
+}
 
-	result, err = CalculatePostfix("2 +")
+func (s *MySuite) TestCalculatePostfixOperandsError(cs *C) {
+	_, err := CalculatePostfix("2 +")
 	cs.Assert(err, ErrorMatches, "invalid expression: not enough operands")
+}
 
-	result, err = CalculatePostfix("--4 2 - 3bd * 5 +")
+func (s *MySuite) TestCalculatePostfixSymbolError1(cs *C) {
+	_, err := CalculatePostfix("--4 2 - 3bd * 5 +")
 	cs.Assert(err, ErrorMatches, "invalid expression: unsupported symbol")
+}
 
-	result, err = CalculatePostfix("2 a +")
+func (s *MySuite) TestCalculatePostfixSymbolError2(cs *C) {
+	_, err := CalculatePostfix("2 a +")
 	cs.Assert(err, ErrorMatches, "invalid expression: unsupported symbol")
+}
 
-	result, err = CalculatePostfix("2 3 5 +")
+func (s *MySuite) TestCalculatePostfixExtraOperandsError(cs *C) {
+	_, err := CalculatePostfix("2 3 5 +")
 	cs.Assert(err, ErrorMatches, "invalid expression: incorrect number of operands")
 }
 
